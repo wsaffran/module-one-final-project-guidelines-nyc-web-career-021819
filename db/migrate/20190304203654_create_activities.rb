d6f882c0bbf0ca2bfde840e92dacbb4f976a894d
adamjgordon28 <adamjgordon24@gmail.com>
2019-03-04 16:10:33 -0500

    finished creating database model structures

---
+++ db/migrate/20190304203654_create_activities.rb
@@ -0,0 +1,11 @@
+class CreateActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.float :accessibility
+      t.string :type
+      t.integer :participants
+      t.float :price
+    end
+  end
+end
---
+++ db/migrate/20190304203716_create_user_activities.rb
@@ -0,0 +1,8 @@
+class CreateUserActivities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :user_activities do |t|
+      t.integer :user_id
+      t.integer :activity_id
+    end
+  end
+end
