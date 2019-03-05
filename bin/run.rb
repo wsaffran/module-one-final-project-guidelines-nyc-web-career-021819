d0d322c35ef94041c96982a177631b2e01686c96
github username <github email address>
2019-03-05 16:47:06 -0500

    Refactored code and added option menu

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,11 @@
 require_relative '../config/environment'
 
-
-
-
 welcome_user
-create_or_find_user_and_choose_activity
+get_username
+find_or_create_user
+puts_list_of_categories
+select_category
+what_next?
+what_next_selections
+
+# CLI.new.welcome_user
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,55 +1,167 @@
 require "pry"
+#
+# class CLI
+#   attr_reader :user
+#
+#   def run_program
+#    welcome_user
+#    ...
+     # @user = User.create(...)
+#   end
+#
+#   def welcome_user
+#   end
+# end
+
+# CLI.new.run_program
+
+
+@user = nil
+@activity = nil
 
 def welcome_user
-  puts "Welcome to BoredQuench: the Gatorade for Boredom. \nYou must be bored."
+  puts "Welcome to BoredQuench: the Gatorade for Boredom. \nYou must be bored.\n"
 end
 
-def user_in_database?(username)
-  if User.find_by(name: username) != nil
+#helper method
+def user_in_database?(user)
+  if User.find_by(name: user) != nil
     true
   else
     false
   end
 end
 
-def create_or_find_user_and_choose_activity
-  puts "Please enter username:"
-  response = gets.chomp.downcase
-  if user_in_database?(response)
-    puts "Welcome back, #{response.capitalize}!"
-    puts "What would you like to do today?"
-    list_of_categories
-    category = gets.chomp.downcase
-    a = get_activity_from_api(category)
-    user = User.find_by(name: response)
-    UserActivity.create(user_id: user.id, activity_id: a.id)
+def get_username
+  print "Please enter username(eg. sharktazer29, aisforawesome123):"
+end
+
+def find_or_create_user
+  username = gets.chomp
+  if user_in_database?(username)
+    puts "Welcome back, #{username}"
+    @user = User.find_by(name: username)
   else
-    puts "Welcome, #{response.capitalize}!"
-    User.create(name: response)
-    list_of_categories
-    category = gets.chomp.downcase
-    a = get_activity_from_api(category)
-    UserActivity.create(user_id: User.find_by(name: response).id, activity_id: a.id)
+    puts "Welcome, #{username}"
+    @user = User.create(name: username)
+  end
+  # @user = username
+end
+
+### Add function to ask user if they would like
+### to see their activity database
+
+# helper method
+def get_category
+  category = gets.chomp.downcase
+  category
+end
+
+def select_category
+  print "\nenter here: "
+  create_activity_from_api(get_category)
+  puts ""
+end
+
+def what_next?
+  puts "\nwhat number would you like to do?"
+  puts "1. complete activity
+2. find a new activity
+3. view completed activities
+4. exit\n"
+
+end
+
+def what_next_selections
+  answer = gets.chomp
+  if answer != "1" && answer != "2" && answer != "3" && answer != "4"
+    puts "\nHello?!?!? ARE YOU BLIND??? ENTER A NUMBER 1 - 4"
+    what_next?
+    what_next_selections
+  elsif answer == "1"
+    add_to_activities
+    puts "\nActivity added to your Activities Database"
+    what_next?
+    what_next_selections
+  elsif answer == "2"
+    puts ""
+    puts_list_of_categories
+    select_category
+    what_next?
+    what_next_selections
+  elsif answer == "3"
+    view_user_activities
+    puts ""
+    what_next?
+    what_next_selections
+
+  elsif answer == "4"
+    puts "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    puts "\nCongratulations #{@user.name}! You're no longer bored! ... for now... \n"
   end
 end
 
-def get_activity_from_api(option)
-  random_activity = RestClient.get("http://www.boredapi.com/api/activity?type=#{option}")
-  activity_hash = JSON.parse(random_activity)
-  puts activity_hash["activity"].downcase
-  x = Activity.find_or_create_by(name: activity_hash["activity"], accessibility: activity_hash["accessibility"], category: activity_hash["type"], participants: activity_hash["participants"], price: activity_hash["price"])
-  return x
+# method 1 to what_next_selections
+def add_to_activities
+  UserActivity.find_or_create_by(user_id: @user.id, activity_id: @activity.id)
 end
 
-def list_of_categories
-  puts "Please specify the type of activity you'd like to try:
-     -Relaxation
-     -Cooking
-     -Education
-     -Social
-     -Charity
-     -Busywork
-     -DIY
-     -Music
-     -Recreational"
+# method 3 to view all activities ***MAKE MORE EFFIECIENT***
+def view_user_activities
+  puts "\nYour Activities:\n"
+  user_id = User.find_by(name: @user.name).id
+  useractivity = UserActivity.where(user_id: @user.id)
+  # binding.pry
+  useractivity.each_with_index do |useractivity, index|
+    puts "#{index+1}. #{Activity.find_by(id: useractivity.activity_id).name}"
+  end
+
 end
+
+def puts_list_of_categories
+  puts "\nPlease specify the type of activity you'd like to try:
+  -Relaxation
+  -Cooking
+  -Education
+  -Social
+  -Charity
+  -Busywork
+  -DIY
+  -Music
+  -Recreational"
+end
+
+def create_activity_from_api(option)
+  random_activity = RestClient.get("http://www.boredapi.com/api/activity?type=#{option}")
+  activity_hash = JSON.parse(random_activity)
+  puts "\nActivity:           #{activity_hash["activity"]}
+
+  accessibility:      #{10 - activity_hash["accessibility"]*10}/10
+  participants:       #{activity_hash["participants"]}
+  price:              #{activity_hash["price"]*10}/10"
+
+  @activity = Activity.find_or_create_by(name: activity_hash["activity"], accessibility: activity_hash["accessibility"], category: activity_hash["type"], participants: activity_hash["participants"], price: activity_hash["price"])
+  puts ""
+end
+
+
+# def create_or_find_user_and_choose_activity
+#   puts "Please enter username:"
+#   response = gets.chomp.downcase
+#   if user_in_database?(response)
+#     puts "Welcome back, #{response.capitalize}!"
+#     puts "What would you like to do today?"
+#     list_of_categories
+#     category = gets.chomp.downcase
+#     a = create_activity_from_api(category)
+#     user = User.find_by(name: response)
+#     UserActivity.create(user_id: user.id, activity_id: a.id)
+#   else
+#     puts "Welcome, #{response.capitalize}!"
+#     User.create(name: response)
+#     list_of_categories
+#     category = gets.chomp.downcase
+#     a = create_activity_from_api(category)
+#     UserActivity.create(user_id: User.find_by(name: response).id, activity_id: a.id)
+#   end
+# end
