a8f753870e6487b75b98636c276a1c3f3a3132cb
github username <github email address>
2019-03-06 16:51:28 -0500

    added progress bar and snarky comments

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,7 @@
 require_relative '../config/environment'
 
+require "ruby-progressbar"
+
+progressbar = ProgressBar.create(title: "Bored?", progress_mark: "~")
+100.times { progressbar.increment; sleep 0.01};
 CLI.new.run_program
